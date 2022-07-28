7d19509793fc7d612f36cdec07d6bcad884cd013
Aj9291 <javvaji.python@gmail.com>
2022-07-28 10:17:41 +0530

    my first
    first commit

---
+++ MVCFirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFirstProject.Models;
+
+namespace MVCFirstProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 1001,
+                EmployeeName = "Achyuthrao J",
+                EmployeeLocation = "Hyderabad"
+            };
+
+            return View(employee);
+        }
+    }
+}
